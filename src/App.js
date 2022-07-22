aab7a5a2a268ba337d166c9bc0abda85b79863bb
Pratham <bhagatpratham101@gmail.com>
2022-07-22 19:04:07 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import BarChart from "./components/BarChart";
+import LineChart from "./components/LineChart";
+import PieChart from "./components/PieChart";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <PieChart />
+      <LineChart />
     </div>
   );
 }
---
+++ src/components/BarChart.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+import { fetchDailyData } from "./Data";
+import Chart from "chart.js/auto";
+import { Line, Bar } from "react-chartjs-2";
+
+const BarChart = () => {
+  const [dailyData, setDailyData] = useState([]);
+
+  const fetchApi = async () => {
+    const dailyData = await fetchDailyData();
+    setDailyData(dailyData);
+  };
+
+  useEffect(() => {
+    fetchApi();
+  }, []);
+
+  const lineChart = dailyData[0] ? (
+    <Line
+      data={{
+        labels: dailyData.map(({ date }) =>
+          new Date(date).toLocaleDateString()
+        ),
+        datasets: [
+          {
+            data: dailyData.map((data) => data.confirmed),
+            label: "Infected",
+            borderColor: "rgb(0, 217, 255)",
+            fill: true,
+          },
+          {
+            data: dailyData.map((data) => data.deaths),
+            label: "Deaths",
+            borderColor: "red",
+            backgroundColor: "rgb(255, 0, 0)",
+            fill: true,
+          },
+        ],
+      }}
+    />
+  ) : null;
+
+  // const barChart = confirmed ? (
+  //   <Bar
+  //     data={{
+  //       labels: ["Infetced", "Deaths", "Recoverd"],
+  //       datasets: [
+  //         {
+  //           label: "People",
+  //           backgroundColor: [
+  //             "rgb(0, 217, 255)",
+  //             "rgb(255, 0, 0)",
+  //             "rgb(0, 255, 21)",
+  //           ],
+  //           data: [confirmed.value, deaths.value, recovered.value],
+  //         },
+  //       ],
+  //     }}
+  //     options={{
+  //       legend: { display: false },
+  //       title: { display: true, text: `Current State is ${country}` },
+  //     }}
+  //   />
+  // ) : null;
+
+  return (
+    <>
+      <div className="linechart_container">{lineChart}</div>
+    </>
+  );
+};
+
+export default BarChart;
